c880c33d3687816daf1d78ff9864b8cbc250abca
M Nur Fauzan W <31560585+virgorasion@users.noreply.github.com>
2017-10-30 16:05:17 +0700

    baru lagi as

--- laravel/resources/views/Layouts/Master.blade.php
+++ laravel/resources/views/Layouts/Master.blade.php
@@ -269,9 +269,7 @@ desired effect
             <li><a href="#">Link in level 2</a></li>
             <li><a href="#">Link in level 3</a></li>
             <li><a href="#">Link in level 4</a></li>
-            <li><a href="#">Link in level 3</a></li>
-            <li><a href="#">Link in level 1</a></li>
-            <li><a href="#">Link in level 3</a></li>
+         
           </ul>
         </li>
       </ul>
