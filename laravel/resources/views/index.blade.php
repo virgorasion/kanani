cf3596af48da101c3ff1e73936eccd6bf26e4f1c
M Nur Fauzan W <31560585+virgorasion@users.noreply.github.com>
2017-11-01 00:50:59 +0700

    new welcome

---
+++ laravel/resources/views/index.blade.php
@@ -0,0 +1,25 @@
+@extends('Layouts.Master')
+
+@section('content')
+    <!-- Content Wrapper. Contains page content -->
+  <div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+      <h1>
+        Page Header
+        <small>Optional description</small>
+      </h1>
+      <ol class="breadcrumb">
+        <li><a href="#"><i class="fa fa-dashboard"></i> Level</a></li>
+        <li class="active">Here</li>
+      </ol>
+    </section>
+
+    
+      </div>
+      <!-- /.row -->
+
+    <!-- /.content -->
+
+  <!-- /.content-wrapper -->
+@endsection
