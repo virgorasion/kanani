be33728c7bd6756784cb4bc7d246c054fddf0c3a
M Nur Fauzan W <31560585+virgorasion@users.noreply.github.com>
2017-11-02 12:55:55 +0700

    new routes

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -15,6 +15,7 @@ Route::get('/', function () {
     return view('index');
 });
 
+//Routes Master
 Route::get('/view_dokter' ,'DokterCtrl@index');
 Route::get('/view_jenisrawat' ,'jenisrawatCtrl@index');
 Route::get('/view_obat' ,'ObatCtrl@index');
@@ -26,13 +27,25 @@ Route::get('/view_userfacial' ,'UserfacialCtrl@index');
 Route::get('/view_level' ,'LevelCtrl@index');
 Route::get('/view_level/setting' ,'LvlsettingCtrl@index');
 Route::get('/view_satuan' ,'SatuanCtrl@index');
+
 //start form nathan
 Route::get('/view_daftar_pasien' , 'DaftarPasien@index');
 Route::get('/view_facial_pasien' , 'FacialPasien@index');
 Route::get('/view_rekam_medis' , 'RekamMedis@index');
+//end nathan
+
+//Routes Transaksi
+Route::get('/view_daftar_perawatan' ,'DaftarPerawatan@index');
+Route::get('/view_beli_obat' ,'BeliObatCtrl@index');
+Route::get('/view_jual_obat' ,'JualObatCtrl@index');
+Route::get('/view_stock_opname' ,'StockOpnameCtrl@index');
+Route::get('/view_penjualan_resep' ,'JualResepCtrl@index');
+Route::get('/view_penulisan_resep' ,'TulisResepCtrl@index');
+Route::get('/view_konversi' ,'KonversiCtrl@index');
+
+//Routes Laporan
+Route::get('/Laporan_absensi' , 'LaporanCtrl@index');
 Route::get('/view_lap_foto' , 'LaporanFoto@index');
 Route::get('/lap_stock_opname' , 'LaporanOpname@index');
 Route::get('/lap_konversi' , 'LaporanKonversi@index');
 Route::get('/view_inventory_resep' , 'LaporanResep@index');
-//end nathan
-Route::get('/Laporan_absensi' , 'LaporanCtrl@index');
